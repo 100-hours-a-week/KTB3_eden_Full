1631f13e71dd7d4443cd0cf3ead795a6ec2a76a5
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-09-21 20:25:28 +0900

    refactor: Scanner 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 public class InputView {
 
     private final InputParser inputParser;
+    private final Scanner scanner = new Scanner(System.in);
 
     public InputView(InputParser inputParser) {
         this.inputParser = inputParser;
     }
 
     public List<String> readStudent() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("본인의 이름과 과정명을 입력해주세요.(이정원-풀스택, 이정원-인공지능, 이정원-클라우드)");
         String input = scanner.nextLine();
         inputParser.validateStudentInput(input);
@@ -26,14 +26,12 @@ public class InputView {
     }
 
     public int readOptionAnswer() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("정답번호를 입력하세요.");
         String input = scanner.nextLine();
         return inputParser.validateOptionInput(input);
     }
 
     public int readQuizType() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("진행할 문제 번호를 입력해주세요.");
         String input = scanner.nextLine();
         inputParser.validateQuizTypeInput(input);
