c0837dfbaa7fdd358359efdc7d55fdabb6e8cb0e
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-09-21 20:28:49 +0900

    feat: 예외 타입 추가

--- src/main/java/exception/QuizExceptionType.java
+++ src/main/java/exception/QuizExceptionType.java
@@ -6,7 +6,8 @@ public enum QuizExceptionType {
     EMPTY_SEPARATOR("이름과 과정명 사이에 - 를 입력해야합니다."),
     INVALID_INPUT_NAME_COURSE("이름이나 코스명이 제대로 입력되지않았습니다."),
     INVALID_INPUT_COURSE("수강명 이름이 잘못되었습니다."),
-    INVALID_INPUT_QUIZ_TYPE("명시된 퀴즈 번호를 입력해주세요"),
+    INVALID_INPUT_QUIZ_TYPE("퀴즈번호는 숫자만 입력해주세요"),
+    INVALID_INPUT_QUIZ_NUMBER("퀴즈번호 범위에 맞게 입력해주세요"),
     NOT_FOUND_STUDENT("해당 하는 학생을 찾을 수 없습니다.");
 
     private String message;
